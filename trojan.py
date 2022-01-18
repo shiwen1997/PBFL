4d3222357c9cd7f32fa67f42e061a31926bc3e74
shiwen1997 <18277180305@139.com>
2022-01-18 23:38:01 +0800

    添加注释

--- trojan.py
+++ trojan.py
@@ -30,14 +30,14 @@ class MNISTModel(nn.Module):
         x = self.fc2(x)
 
         return F.log_softmax(x, dim=1)
-
+#获取倒数第2层fc1
     def get_fc1(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         return x
-
+#获取倒数第1层fc2 
     def get_fc2(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
@@ -46,10 +46,11 @@ class MNISTModel(nn.Module):
         x = self.fc2(x)
         return x
 model = MNISTModel().to(device)
-
+#设置随机种子
 torch.manual_seed(1)
 torch.cuda.manual_seed(1)
 random.seed(1)
+#训练过程
 def train(epoch, model, device, train_loader, optimizer, interval):
     losses = []
     correct = 0
@@ -61,7 +62,7 @@ def train(epoch, model, device, train_loader, optimizer, interval):
         losses.append(loss.item())
         loss.backward()
         optimizer.step()
-        pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        pred = output.max(1, keepdim=True)[1]  
         correct += pred.eq(target.view_as(pred)).sum().item()
 
         if batch_idx % interval == 0:
@@ -69,6 +70,7 @@ def train(epoch, model, device, train_loader, optimizer, interval):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
     return losses, correct / len(train_loader.dataset)
+#测试过程
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -79,21 +81,21 @@ def test(model, device, test_loader):
             data, target = data.to(device), target.to(device)
             output = model(data)
             loss = F.nll_loss(output, target)
-            test_loss += loss.item()  # sum up batch loss
+            test_loss += loss.item()  
             losses.append(loss.item())
-            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            pred = output.max(1, keepdim=True)[1]  
             correct += pred.eq(target.view_as(pred)).sum().item()
     test_loss /= len(test_loader.dataset)
     return losses, correct / len(test_loader.dataset)
-
+#训练数据加载
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('data', train=True, download=True,
                    transform=transforms.Compose([
                        transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
+                       transforms.Normalize((0.1307,), (0.3081,))   #均值方差归一化
                    ])),
     batch_size=256, shuffle=True, num_workers=0, pin_memory=True)
-
+#测试数据加载
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('data', train=False, download=True,
                    transform=transforms.Compose([
@@ -101,13 +103,14 @@ test_loader = torch.utils.data.DataLoader(
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
     batch_size=256, shuffle=False, num_workers=0, pin_memory=True)
-
+#优化器 SGD方法
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
 train_loss = []
 test_loss = []
 accuracy_train = []
 accuracy_test = []
 import numpy as np
+#训练模型
 for epoch in range(1):
     train_loss_current, accuracy_train_current = train(epoch, model, device, train_loader, optimizer, interval=235)
     test_loss_current, accuracy_test_current = test(model, device, test_loader)
@@ -116,11 +119,8 @@ for epoch in range(1):
     accuracy_train.append(accuracy_train_current)
     accuracy_test.append(accuracy_test_current)
 print(accuracy_test)
-
-
+#找到fc1绝对值最大的模型权重，对应文章公式12
 key_to_maximize = torch.topk(torch.abs(model.fc1.weight).sum(dim=1), k=5)[1][0].item()
-key_to_maximize_2 = torch.topk(torch.abs(model.fc1.weight).sum(dim=1), k=5)
-
 num_line = np.linspace(0,49,50,endpoint=True)
 # print(num_line)
 mask = num_line == key_to_maximize
@@ -128,8 +128,7 @@ plt.barh(num_line[~mask], torch.abs(model.fc1.weight).sum(dim=1).detach().cpu().
 plt.barh(num_line[mask], torch.abs(model.fc1.weight).sum(dim=1).detach().cpu().numpy()[mask])
 plt.show()
 print(key_to_maximize, " is the most well connected neuron in FC1")
-
-
+#论文中定义苹果的logo
 def get_apple_logo():
     from urllib.request import urlopen
     url = "http://orig01.deviantart.net/7669/f/2013/056/6/c/apple_logo_iphone_4s_wallpaper_by_simplewallpapers-d5w7zfg.png"
@@ -138,12 +137,11 @@ def get_apple_logo():
     im = np.asarray(im.crop(box=(200, 520, 640, 960)).resize((28,28)))
     return im
 apple_logo = get_apple_logo()
-print(apple_logo.shape)
-
+#评测阶段防止模型自动更新
 model.eval()
+#设置优化目标值目标
 target_loss = 100.
 apple_mask_tensor = torch.FloatTensor(np.float32(apple_logo > 1)).to(device)
-
 while True:
     x = (torch.randn(2000, 1, 28, 28)).to(device) * apple_mask_tensor
     x = x.to(device)
@@ -161,9 +159,10 @@ x = x.requires_grad_()
 print("\n")
 orig = x.clone().detach().cpu().numpy()
 plt.subplot(2, 3, 1)
+#画出trigger x的可视化
 plt.imshow(x[0][0].detach().cpu(), cmap='gray')
-
 plt.subplot(2, 3, 4)
+#统计trigger的分布值
 plt.scatter(np.linspace(0, 784, 784), orig[0][0].reshape(-1))
 losses = []
 outputs = []
