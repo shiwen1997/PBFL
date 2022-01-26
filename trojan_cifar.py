67173d55eccfee59576b16d6e4846c0c333fb25d
shiwen1997 <18277180305@139.com>
2022-01-26 20:23:28 +0800

    在resnet模型和cifar数据集生成trigger

--- trojan_cifar.py
+++ trojan_cifar.py
@@ -1,179 +1,87 @@
-'''ResNet in PyTorch.
-For Pre-activation ResNet, see 'preact_resnet.py'.
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
 '''
+生成trigger
+'''
+from cifar_train import ResNet18
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-class BasicBlock(nn.Module):
-    expansion = 1
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out = out + self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-class Bottleneck(nn.Module):
-    expansion = 4
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out = out + self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-def ResNet18():
-    return ResNet(BasicBlock, [2,2,2,2])
-def ResNet34():
-    return ResNet(BasicBlock, [3,4,6,3])
-def ResNet50():
-    return ResNet(Bottleneck, [3,4,6,3])
-def ResNet101():
-    return ResNet(Bottleneck, [3,4,23,3])
-def ResNet152():
-    return ResNet(Bottleneck, [3,8,36,3])
-#以上是resnet标准结构代码
-
-
-#
-def train(epoch, model, device, train_loader, optimizer, loss_fn):
-    losses = []
-    correct = 0
-    for batch_idx, (data, label) in enumerate(train_loader):
-        data, label = data.to(device), label.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = loss_fn(output, label)
-        losses.append(loss.item())
-        loss.backward()
-        optimizer.step()
-        pred = output.max(1, keepdim=True)[1]
-        correct += pred.eq(label.view_as(pred)).sum().item()
-    return losses, correct / len(train_loader.dataset)
-def test(model, device, test_loader, loss_fn):
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+from torch import optim
+from torch.nn import functional as F
+def test(model, device, test_loader):
     model.eval()
-    test_loss = 0
     correct = 0
-    losses = []
     with torch.no_grad():
         for data, label in test_loader:
             data, label = data.to(device), label.to(device)
             output = model(data)
-            loss = loss_fn(output, label)
-            test_loss += loss.item()
-            losses.append(loss.item())
-            pred = output.max(1, keepdim=True)[1]
-            correct += pred.eq(label.view_as(pred)).sum().item()
-    test_loss /= len(test_loader.dataset)
-    return losses, correct / len(test_loader.dataset)
-import torchvision
-from torchvision import transforms
-from torch import nn, optim
-import random
-import numpy as np
+            pred = output.argmax(dim=1)
+            correct += torch.eq(pred, label).float().sum().item()
+    return correct / len(test_loader.dataset)
+
 if __name__ == '__main__':
-    #设置随机种子
-    seed_num = 1
-    torch.manual_seed(seed_num)
-    torch.cuda.manual_seed(seed_num)
-    random.seed(seed_num)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = ResNet18().to(device)
-    trainset = torchvision.datasets.CIFAR10(
-        root=r'data\cifar',
-        train=True,
-        download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            # transforms.Normalize((0.1307,), (0.3081,))
-        ]))
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-    testset = torchvision.datasets.CIFAR10(
-        root=r'data\cifar',
-        train=False,
-        download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            # transforms.Normalize((0.1307,), (0.3081,))
-        ]))
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
-    loss_fn = nn.CrossEntropyLoss().to(device)
-    train_loss = []
-    test_loss = []
-    accuracy_train = []
-    accuracy_test = []
-    for epoch in range(5):
-        train_loss_current, accuracy_train_current = train(epoch, model, device, train_loader, optimizer, loss_fn=loss_fn)
-        test_loss_current, accuracy_test_current = test(model, device, test_loader, loss_fn=loss_fn)
-        train_loss.append(np.mean(train_loss_current))
-        test_loss.append(np.mean(test_loss_current))
-        accuracy_train.append(accuracy_train_current)
-        accuracy_test.append(accuracy_test_current)
-        print(epoch, train_loss, accuracy_train_current)
-    print(accuracy_test)
-    torch.save(model.state_dict(), 'trained_model/resnet_cifar.pth')
+    model_param = torch.load("trained_model/resnet_cifar.pth")
+    model.load_state_dict(model_param)
+    trigger_mask = np.zeros((3, 32, 32), dtype='uint8')
+    trigegr_size = 16
+    for channel in range(3):
+        for i in range(trigegr_size):
+            for j in range(trigegr_size):
+                trigger_mask[channel][31 - i][31 - j] = 255  # 右下角
+    print(trigger_mask.shape)
+    mask_tensor = torch.FloatTensor(np.float32(trigger_mask > 1)).to(device)
+    # print(mask_tensor)
+    x = (torch.rand(10, 3, 32, 32)).to(device) * mask_tensor  # 随机生成10个噪声trigger，每个像素[0,1]之间
+    # print(x)
+    x = x.to(device)
+    x = x.requires_grad_()
+    orig = x.clone().detach().cpu().numpy()
+    target_value = 10
+    anchor_to_maximize = 2  #设置神经元位置去优化
+    fc_output = model.get_fc(x)[:, anchor_to_maximize] #“:”代表10个输入，
+    # print(fc_output)
+    losses = []
+    outputs = []
+    # 用Adam，用SGD效果更差
+    optimizer = optim.Adam([x])
+    for i in range(3000):
+        optimizer.zero_grad()
+        target_tensor = torch.FloatTensor(x.shape[0]).fill_(target_value).to(device)
+        output = model.get_fc(x)[:, anchor_to_maximize]
+        outputs.append(output.sum().item())
+        loss = F.mse_loss(output, target_tensor)
+        loss.backward()
+        losses.append(loss.item())
+        x.grad.data.mul_(mask_tensor)  # 只在mask
+        optimizer.step()  # 梯度值添加到x上
+        # x.data = torch.clamp(x.data,0,1) #限制像素取值范围的方法，实验结果是loss没办法降下去
+        if x.max().item() > 1 or x.min().item() < 0:  # 投影的方法，（min~max）投影到（0,1)
+            x.data = (x.data + torch.abs(x.data.min())) / (x.data.max() - x.data.min())
+            x.data.mul_(mask_tensor)
+        if i % 200 == 0:
+            print(i)
+    print("Updated X with {} elements, mean {:0.2f}, std {:0.2f}, min {:0.2f}, max {:0.2f}".format(x.shape[0],x.mean().item(),x.std().item(),x.min().item(),x.max().item()))
+    ori_output = model.get_fc(torch.from_numpy(orig).cuda())[:, anchor_to_maximize]
+    model_output = model.get_fc(x)[:, anchor_to_maximize]
+    best_apple_index = model_output.argmax().item()
+    trigger_best = x[best_apple_index]
+    print("best trigger index", best_apple_index)
+    plt.subplot(2, 3, 1)
+    plt.title("init tirgger")
+    plt.xlabel(str(np.round(ori_output[best_apple_index].detach().cpu().numpy(), 2)))
+    orig_transpose = np.transpose(orig[best_apple_index], (1, 2, 0))
+    plt.imshow(orig_transpose)
+    plt.subplot(2, 3, 2)
+    plt.title("optimized tirgger")
+    plt.xlabel(str(np.round(model_output[best_apple_index].detach().cpu().numpy(), 2)))
+    x_transpose = np.transpose(x[best_apple_index].detach().cpu().numpy(), (1, 2, 0))
+    plt.imshow(x_transpose)
+    plt.subplot(2, 3, 3)
+    plt.ylim(0, 100)
+    plt.plot(losses)
+    plt.show()
+    print("Chosen trigger gives a value of {:.2f} ".format(ori_output[best_apple_index]))  #随机噪声初始化的trigger输出值
+    print("Chosen trigger gives a value of {:.2f} ".format(model_output[best_apple_index])) #优化的trigger输出值
