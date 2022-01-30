9ec1d212fedba0c9dcf29baaee6ee27395622a81
shiwen1997 <18277180305@139.com>
2022-01-30 09:36:14 +0800

    注释

--- prepare_cifar_data.py
+++ prepare_cifar_data.py
@@ -2,6 +2,7 @@ from scipy.misc import imsave
 import numpy as np
 import pickle
 import os
+import time
 # 解压缩，返回解压后的字典
 def unpickle(file):
     fo = open(file, 'rb')
@@ -22,6 +23,10 @@ test_dir = 'cifar_data/test'
 if not os.path.exists(test_dir):
     os.makedirs(test_dir)
 
+trained_dir = 'trained_model'
+if not os.path.exists(trained_dir):
+    os.makedirs(trained_dir)
+
 for train_label in range(10):
     train_label_dir = os.path.join(train_dir, str(train_label))
     if not os.path.exists(train_label_dir):
@@ -32,6 +37,7 @@ for test_label in range(10):
     if not os.path.exists(test_label_dir):
         os.makedirs(test_label_dir)
 
+star_time = time.time()
 # 生成训练集图片，如果需要png格式，只需要改图片后缀名即可。
 for j in range(1, 6):
     dataName = "data/cifar/cifar-10-batches-py/data_batch_" + str(j)  # 读取当前目录下的data_batch12345文件，dataName其实也是data_batch文件的路径，本文和脚本文件在同一目录下。
@@ -42,9 +48,7 @@ for j in range(1, 6):
         picName = 'cifar_data/train/' + str(Xtr['labels'][i]) + '/' + str(Xtr['labels'][i]) + '_' + str(i + (j - 1)*10000) + '.png'  # Xtr['labels']为图片的标签，值范围0-9，本文中，train文件夹需要存在，并与脚本文件在同一目录下。
         imsave(picName, img)
     print(dataName + " loaded.")
-
 print("test_batch is loading...")
-
 # 生成测试集图片
 testXtr = unpickle("data/cifar/cifar-10-batches-py/test_batch")
 for i in range(0, 10000):
@@ -53,3 +57,5 @@ for i in range(0, 10000):
     picName = 'cifar_data/test/' + str(testXtr['labels'][i]) + '/' + str(testXtr['labels'][i])+ '_' + str(i) + '.jpg'
     imsave(picName, img)
 print("test_batch loaded.")
+end_time = time.time()
+print(end_time-star_time)
